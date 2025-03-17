4502bee7605e3dbd93606003830189d5492033e9
dumdumik <fjdvgt149@gmail.com>
2025-03-17 17:16:55 +0700

    zanovo

--- js/main.js
+++ js/main.js
@@ -3,7 +3,22 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        description: "A pair of warm, fuzzy socks"
-
+        description: "A pair of warm, fuzzy socks",
+        image: "./assets/vmSocks-green-onWhite.jpg",
+        altText: "A pair of socks",
+        inStock: true,
+        link: "More products like this",
+        OnSale: false,
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
     }
 })
