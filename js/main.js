3ecc2a4eb9c986e7075700658ef8bd0171ab0827
dumdumik <fjdvgt149@gmail.com>
2025-03-17 17:17:44 +0700

    10

--- js/main.js
+++ js/main.js
@@ -3,22 +3,50 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
+        brand: 'Vue Mastery',
         description: "A pair of warm, fuzzy socks",
         image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
-        inStock: true,
+        inStock: false,
         link: "More products like this",
-        OnSale: false,
+        onSale: true,
         variants: [
             {
                 variantId: 2234,
-                variantColor: 'green'
+                variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
             },
             {
                 variantId: 2235,
-                variantColor: 'blue'
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
             }
         ],
-        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
-    }
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+        cart: 0,
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        },
+        ubratOnCart() {
+            if (this.cart > 0) {
+                this.cart -= 1;
+            } else {
+                alert("Бро, корзина и так пуста куда минусуешь");
+            }
+        },
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        sale() {
+            return this.onSale ? `${this.brand} ${this.product} is on sale!` : `${this.brand} ${this.product} is not on sale.`;
+        }
+    },
+
 })
